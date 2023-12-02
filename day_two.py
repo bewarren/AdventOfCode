2cb08e326e2d83ff56e584fc003507a6061d2f76
Benjamin Warren <benwarren5020@gmail.com>
2023-12-02 21:36:42 +0200

    day 2 part 2

--- day_two.py
+++ day_two.py
@@ -29,7 +29,6 @@ def check_game(game):
             total += int(num)
 
             if (int(num) > data[colour]):
-                print(id, num, data[colour])
                 return 0
 
         if (total >= 12 + 13 + 14):
@@ -38,10 +37,41 @@ def check_game(game):
     # print (id)
     return int(id)
 
+def min_set(game): 
+    game_data = game.split(":")
+
+    sets = game_data[1].split(";")
+
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+
+    for set in sets:
+        cubes = set.split(",")
+
+        for subset in cubes:
+            values = subset.strip().split(" ")
+            
+            num, colour = int(values[0]), values[1]
+
+            if (colour == "red"):
+                if (min_red < num):
+                    min_red = num 
+            
+            elif (colour == "blue"):
+                if (min_blue < num):
+                    min_blue = num 
+            
+            else:
+                if (min_green < num):
+                    min_green = num
+    return min_red * min_green * min_blue
+
+
 anwser = 0
 
 for game in data_into_list:
 
-    anwser += check_game(game)
+    anwser += min_set(game)
 
 print(anwser)
