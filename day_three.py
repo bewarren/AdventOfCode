d6bc60897f0f0760bb2361fe133bc84db6108d29
Benjamin Warren <benwarren5020@gmail.com>
2023-12-04 16:44:00 +0200

    day 3 pt -- late

--- day_three.py
+++ day_three.py
@@ -45,34 +45,82 @@ def get_numbers(line):
      return numbers
              
              
-             
-             
-         
+def get_gear_location(line):
+    gears = []   
+    for i, symbol in enumerate(line.replace(" ", "")):
+            if (symbol == "*"):
+                gears.append(i)
+    return gears
+
+def getNumbers(lines): 
+    full_numbers = []
+    for line in lines:
+        full_numbers.extend(get_numbers(line))
+
+    return full_numbers
+
 
 length_of_list = len(data_into_list)
 
 anwser = 0
-
+        
 for i, line in enumerate(data_into_list):
-    numbers = get_numbers(line.replace(" ", ""))
+    
 
-    symbol_locations = []
+    gear_locations = get_gear_location(line.replace(" ", ""))
+
+    number_locations = []
 
     if i == 0:
-         symbol_locations = get_symbol_locations(data_into_list[0: 2])
+        number_locations = getNumbers(data_into_list[0: 2])
     elif i == length_of_list -1:
-           
-           symbol_locations = get_symbol_locations(data_into_list[i-1: i+1])
+        number_locations = getNumbers(data_into_list[i-1: i+1])
     else:
-        symbol_locations = get_symbol_locations(data_into_list[i-1: i+2])
+        number_locations = getNumbers(data_into_list[i-1: i+2])
+
+   
+    for gear in gear_locations:
+        numbers = []
+        min_anwser = 0
+        for num in number_locations:
+            
+            if gear in range(int(num["start"])-1, int(num["end"])+2):
+                numbers.append(int(num["number"]))
+        if len(numbers) == 2:
+            min_anwser = numbers[0] * numbers[1]
+            anwser += min_anwser
+
+print(anwser)
+         
+
+# length_of_list = len(data_into_list)
+
+# anwser = 0
+
+
+# for i, line in enumerate(data_into_list):
+#     numbers = get_numbers(line.replace(" ", ""))
+
+#     symbol_locations = []
+
+#     if i == 0:
+#          symbol_locations = get_symbol_locations(data_into_list[0: 2])
+#     elif i == length_of_list -1:
+           
+#            symbol_locations = get_symbol_locations(data_into_list[i-1: i+1])
+#     else:
+#         symbol_locations = get_symbol_locations(data_into_list[i-1: i+2])
 
     
-    for num in numbers:
-        for location in symbol_locations:
-            if location in range(int(num["start"])-1, int(num["end"])+2):
+#     for num in numbers:
+#         for location in symbol_locations:
+#             if location in range(int(num["start"])-1, int(num["end"])+2):
                 
-                anwser += int(num["number"])
-    
+#                 anwser += int(num["number"])
+  
         
-print(anwser)
+# print(anwser)
+
+
+
 
